14f906c75eee409dddaa00deb2a91761bf919358
nick <nicholassam6425@gmail.com>
2025-03-16 04:25:05 -0400

    placeholder ui elements, skeleton for client/host connection

--- Assets/Scripts/Networking/NetworkButtons.cs
+++ Assets/Scripts/Networking/NetworkButtons.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
 using Unity.Netcode;
+using TMPro;
 
 public class NetworkUIManager : MonoBehaviour {
     public Button clientButton;
     public Button hostButton;
     public Button serverButton;
+    public TMP_InputField inputField;
+    public TMP_Text hostText;
 
     void Start() {
         clientButton.onClick.AddListener(StartClient);
@@ -16,11 +19,14 @@ public class NetworkUIManager : MonoBehaviour {
     void StartClient() {
         Debug.Log("Starting Client...");
         NetworkManager.Singleton.StartClient();
+        Debug.Log(inputField.text);
     }
 
     void StartHost() {
         Debug.Log("Starting Host...");
         NetworkManager.Singleton.StartHost();
+        hostText.text = "some text";
+        hostText.gameObject.SetActive(true);
     }
 
     void StartServer() {
